b50b4922cd16b466618e350d6fe8d6316f0e1a50
konar87 <konar87@github.com>
2010-12-23 21:07:12 +0100

    masz

---
+++ KlientSR/KlientSR/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+
+using SimpleConection;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Http;
+
+namespace KlientSR
+{
+    public partial class Form1 : Form
+    {
+        string plik = "";
+        Wiadomosci mm;
+        public Form1()
+        {
+            InitializeComponent(); 
+
+            
+        }
+        //---mechanizm sprawdzania liczbe/litera - usuwa litery
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int pozycja = richTextBox1.SelectionStart;
+                string input = richTextBox1.Text;
+                Match match = Regex.Match(input, @"^([0-9\n]*)$");
+                if (!match.Success) 
+                {
+                    richTextBox1.Text = input.Remove(pozycja - 1, 1);
+                    richTextBox1.SelectionStart = pozycja - 1;
+                }
+            }
+            catch { }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text != "")
+            {
+                DialogResult odp = czyZapisac();
+                if (odp == DialogResult.Cancel)
+                    return;
+                plik = "";
+                richTextBox1.Clear();
+            }
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Plik tekstowy (*.txt)|*.txt";
+            dialog.Multiselect = false;
+            dialog.ShowDialog();
+            if (dialog.FileName != "")
+            {
+                plik = dialog.FileName;
+                StreamReader f = new StreamReader(plik);
+                richTextBox1.Text = f.ReadToEnd();
+                f.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Plik tekstowy (*.txt)|*.txt";
+            dialog.ShowDialog();
+            if(dialog.FileName != "")
+            {
+                plik = dialog.FileName;
+                StreamWriter f = new StreamWriter(plik);
+                f.Write(richTextBox1.Text);
+                f.Close();
+            }
+        }
+
+        private DialogResult czyZapisac()
+        {
+            DialogResult odp = MessageBox.Show("Chcesz zapisać zmiany?", "Notatnik", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+            if (odp == DialogResult.Yes)
+                button1_Click(null, null);
+            return odp;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                mm.rozlaczClient(login);
+            }
+            catch { }
+            if (richTextBox2.Text != "")
+            {
+                DialogResult odp = czyZapisac();
+                if (odp == DialogResult.Cancel)
+                    e.Cancel = true;
+            }
+        }
+        string liczbyO = "";
+        ArrayList posortuj = null;
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            liczbyO = "";
+            Random random = new Random();
+            int imax = int.Parse(textBox4.Text);
+            //int tmp2 = int.Parse(random.Next(100/*int.MaxValue*/ ).ToString());
+            for (int i = 0; i < imax; i++)
+            {
+                for (int j = 0; j < int.Parse(textBox1.Text); j++)
+                {
+                    liczbyO+= random.Next(10).ToString();
+                }
+                
+    
+                    liczbyO += " ";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Plik tekstowy (*.txt)|*.txt";
+            dialog.ShowDialog();
+            if (dialog.FileName != "")
+            {
+                plik = dialog.FileName;
+                StreamWriter f = new StreamWriter(plik);
+                f.Write(richTextBox1.Text);
+                f.Close();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            button4.Enabled = true;
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            button5.Enabled = true;
+        }
+        string login = "dawid";
+        private void button3_Click(object sender, EventArgs e)
+        {
+                string odbiorca = "Server";
+                wyswietlLog("OD: " + login + "\tDO: " + odbiorca + "\nposortuj:\n" + liczbyO+"\n\n");
+                mm.nadajWiadomosc("posortuj", login, odbiorca, liczbyO, 0,0);
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Bufor s;
+            string dane;
+            ArrayList Posortuj;
+            while (true)
+            {
+
+                if (mm != null)
+                {
+                    s = mm.OdbierzWiadomosc(login);
+                    if (s != null)
+                    {
+                        Posortuj = getLista(s.Posortuj);
+                        dane = "OD: " + s.Nadawca + "\tDO: " + s.Odbiorca + "\n" + s.Wiadomosc + "\n"+s.Posortuj+"\n\n";
+                        
+                        wyswietlLog(dane );
+                        if (s.Wiadomosc == "sortuj" && s.Posortuj != null)
+                        {
+                            ArrayList a = Posortuj;
+                            a.Sort();
+                            //s.Posortuj.Sort();
+                            
+                            mm.nadajWiadomosc("posortowano", s.Odbiorca, "Server", getString(a), 0, s.blok);
+                        }
+                        if (s.Wiadomosc == "posortowano" && s.Posortuj != null)
+                        {
+                            mm.nadajWiadomosc("odebrano", s.Odbiorca, "Server", "", 0, 0);
+                        }
+                    }
+                }
+            }
+        }
+
+
+        public void wyswietlLog(String tekst)
+        {
+
+            if (richTextBox3.InvokeRequired)
+                richTextBox3.Invoke(new Action<string>(wyswietlLog), tekst);
+            else
+            {
+                richTextBox3.Focus();
+                richTextBox3.AppendText(tekst);
+                richTextBox3.ScrollToCaret();
+            }
+        }
+
+
+        ArrayList getLista(string dane)
+        {
+            ArrayList lista = new ArrayList();
+            if (dane != null)
+                if (dane.Length > 0)
+                {
+                    MatchCollection matches = Regex.Matches(dane, @"([0-9]+)");
+
+                    foreach (Match match in matches)
+                    {
+
+                        lista.Add((string)match.Value);
+                    }
+                    return lista;
+
+                }
+            return null;
+        }
+        string getString(ArrayList dane)
+        {
+            string lista = "";
+            if(dane!=null)
+            if (dane.Count > 0)
+            {
+                for (int i = 0; i < dane.Count; i++)
+                {
+                    lista += (string)dane[i] + " ";
+                }
+                return lista;
+            }
+            return "";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            HttpChannel d = new HttpChannel();
+            ChannelServices.RegisterChannel(d);
+
+            mm = (Wiadomosci)Activator.GetObject(typeof(SimpleConection.Wiadomosci),
+                                                "http://"+textBox2.Text+":3000/Polaczenie");
+            login = textBox5.Text;
+
+            mm.logowanie(login, "lala");
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+
+    }
+}
---
+++ KlientSR/KlientSR/Rejestrowanie.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace KlientSR
+{
+    partial class Rejestrowanie
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Hasło";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(151, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(87, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(151, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(87, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 48);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Zarejestruj się";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "IP Serwera";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(87, 106);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(105, 20);
+            this.textBox3.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Powtórz hasło";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(87, 80);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(151, 20);
+            this.textBox4.TabIndex = 16;
+            // 
+            // Rejestrowanie
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 200);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Rejestrowanie";
+            this.Text = "Rejestrowanie";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+
+    }
+}
